b132316ba7693bd784f2d4e7229d7ab78c6c6163
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-03-15 23:15:48 +0530

    inherit and created User Model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
 
-# Create your models here.
+
+class User(AbstractUser):
+    display_picture = models.ImageField(upload_to='dpImages')
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
+
+    objects = BaseException()
