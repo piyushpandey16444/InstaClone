de4f91b7fd388b986a3451dd872f9bb453db6f8d
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-03-16 00:22:01 +0530

    started making templates.

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+
+class SignUp(View):
+    template_name = ''
+
+    def get(self, request, *args, **kwargs):
+        return render(request, template_name)
+
+    def post(self, request, *args, **kwargs):
+        pass
