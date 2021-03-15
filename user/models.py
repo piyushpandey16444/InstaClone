5d71d00bb0b628560f6085fc082ee187bcbbb506
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-03-15 23:50:30 +0530

    BaseUser Inherited to make email unique.

--- user/models.py
+++ user/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.base_user import BaseUserManager
+from .managers import CustomUserManager
 
 
 class User(AbstractUser):
     display_picture = models.ImageField(
         upload_to='dpImages', null=True, blank=True)
+    full_name = models.CharField(max_length=250)
+    email = models.EmailField(max_length=254, unique=True)
 
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['full_name',]
+
+    objects = CustomUserManager()
